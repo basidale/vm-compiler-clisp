17569b894fc72400562a9abe3554bb256f44d937
basil.dalie@etu.umontpellier.fr <basil.dalie@etu.umontpellier.fr@j4.fdsetu.infra.umontpellier.fr>
2020-02-03 19:36:44 +0100

    Added vm-condition to master

---
+++ compiler/li2-to-vm/li2-to-vm-condition.lisp
@@ -0,0 +1,31 @@
+(defun compile-condition (comparison then else env src src-dest compiler)
+  (let ((then-label (compiler-increment-label-counter compiler))
+	(end-label (compiler-increment-label-counter compiler)))
+    
+  (append (compile-comparison (car comparison) (cadr comparison) (caddr comparison))
+	  (li2-to-vm-map-compile-expr then)
+	  '((label ,then-label))
+	  (li2-to-vm-map-compile-expr else)
+	  '((label ,end-label)))))
+
+(defun comparison-operator (operator)
+  (assoc operator '((:lt . jlt)
+		    (:le . jle)
+		    (:eq . jeq)
+		    (:ge . jge)
+		    (:gt . jgt))))
+
+(defun is-comparison (expr)
+  (if (comparison-operator (car expr)) t nil))
+
+(defun compile-comparison (operator first-operand second-operand env src src-dest compiler)
+  (flet ((compile-operand (expr register)
+	   (if (or (equal (car expr) :const) (equal (car expr) :arg))
+	       `(move ,(li2-to-vm-compile-expr expr env src src-dest compiler) ,register)
+	       (error "Compile comparison error : operand type is not supported"))))
+    (list (compile-operand first-operand 'R0)
+	  (compile-operand second-operand 'R1)
+	  '(cmp R0 R1)
+	  `(,(cdr (comparison-operator operator)) label))))
+
+;; (compile-comparison :eq '(:const 1) '(:arg x) '(x) nil nil)
